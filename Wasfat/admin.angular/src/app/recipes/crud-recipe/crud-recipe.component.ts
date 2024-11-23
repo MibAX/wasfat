34e0ea580b855873dc31e3eb7a42f237795c0436
MibAX <Instructors@mibax.com>
2024-11-23 18:36:58 +0100

    09.06 - Declaring the recipeFormGroup

--- Wasfat/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
+++ Wasfat/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-crud-recipe',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./crud-recipe.component.scss']
 })
 export class CrudRecipeComponent {
+  recipeFormGroup: FormGroup;
 
 }
