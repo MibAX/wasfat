87878ddcd9e0c4e0871218c3e24a75efd4cd56f1
MibAX <Instructors@mibax.com>
2025-03-09 10:26:37 +0100

    11.21 - Building the Instructions List Component

--- Wasfat/admin.angular/src/app/instructions/instructions-list/instructions-list.component.ts
+++ Wasfat/admin.angular/src/app/instructions/instructions-list/instructions-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { InstructionAdminService, InstructionDto } from '@proxy/instructions';
 
 @Component({
   selector: 'app-instructions-list',
   templateUrl: './instructions-list.component.html',
   styleUrls: ['./instructions-list.component.scss']
 })
-export class InstructionsListComponent {
+export class InstructionsListComponent implements OnInit {
+
+  instructions: InstructionDto[] = [];
+
+  constructor(
+    private instructionAdminSvc: InstructionAdminService,
+    private router: Router) {
+    console.log('InstructionsListComponent > constructor');
+
+  }
+
+  ngOnInit(): void {
+    console.log('InstructionsListComponent > ngOnInit');
+
+    this.instructionAdminSvc.getAllInstructions().subscribe(data => this.instructions = data);
+
+
+  }
+
+  newInstruction(): void {
+    this.router.navigate(["/instructions/create"]);
+  }
+
+  editInstruction(id: number): void {
+    this.router.navigate(["/instructions/edit", id]);
+  }
 
 }
