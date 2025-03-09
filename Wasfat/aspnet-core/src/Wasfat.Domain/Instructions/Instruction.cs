1c5e00776735afe04a85d29b70244b02782da941
MibAX <Instructors@mibax.com>
2025-03-09 05:43:36 +0100

    11.03 - Creating the Instruction Entity Class

---
+++ Wasfat/aspnet-core/src/Wasfat.Domain/Instructions/Instruction.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace Wasfat.Instructions
+{
+    public class Instruction : Entity<int>
+    {
+        public int RecipeId { get; set; }
+        public int Order { get; set; }      // Specifies the order of this instruction within the recipe
+        public string Text { get; set; }
+    }
+}
