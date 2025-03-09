3bdbdf2f869b7a3db8e82a64dfe3407d7527f69d
MibAX <Instructors@mibax.com>
2025-03-09 09:19:19 +0100

    11.16 - Generating the CRUD Instruction Component
       PS> ng generate component instructions/crud-instruction

---
+++ Wasfat/admin.angular/src/app/instructions/crud-instruction/crud-instruction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-instruction',
+  templateUrl: './crud-instruction.component.html',
+  styleUrls: ['./crud-instruction.component.scss']
+})
+export class CrudInstructionComponent {
+
+}
--- Wasfat/admin.angular/src/app/instructions/instructions.module.ts
+++ Wasfat/admin.angular/src/app/instructions/instructions.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { InstructionsRoutingModule } from './instructions-routing.module';
 import { InstructionsListComponent } from './instructions-list/instructions-list.component';
+import { CrudInstructionComponent } from './crud-instruction/crud-instruction.component';
 
 
 @NgModule({
   declarations: [
-    InstructionsListComponent
+    InstructionsListComponent,
+    CrudInstructionComponent
   ],
   imports: [
     CommonModule,
