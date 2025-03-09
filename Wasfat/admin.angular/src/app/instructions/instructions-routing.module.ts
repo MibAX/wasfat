7caf7058cecbee4c3c3be6a845e9251c5b88b45b
MibAX <Instructors@mibax.com>
2025-03-09 09:38:03 +0100

    11.18 - Routing Inside the Instructions Module

--- Wasfat/admin.angular/src/app/instructions/instructions-routing.module.ts
+++ Wasfat/admin.angular/src/app/instructions/instructions-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InstructionsListComponent } from './instructions-list/instructions-list.component';
+import { CrudInstructionComponent } from './crud-instruction/crud-instruction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',
+    component: InstructionsListComponent,
+  },
+  {
+    path: 'create',
+    component: CrudInstructionComponent,
+  },
+  {
+    path: 'edit/:id',
+    component: CrudInstructionComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
