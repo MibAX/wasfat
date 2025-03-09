3569cbf620cbba88f9e85f1716156e22a0394b80
MibAX <Instructors@mibax.com>
2025-03-09 07:29:08 +0100

    11.12 - Defining the Backend Service Interface

---
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/Instructions/IInstructionAppService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Wasfat.Instructions
+{
+    public interface IInstructionAppService : ICrudAppService<
+             InstructionDto,
+             int,
+             PagedAndSortedResultRequestDto>
+    {
+
+        Task<List<InstructionDto>> GetRecentAsync(int count = 3);
+
+        Task<List<InstructionDto>> GetAllInstructionsAsync();
+    }
+}
