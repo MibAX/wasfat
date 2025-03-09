1c092830aa656e4097e2b4a13ca5bad65626e9c7
MibAX <Instructors@mibax.com>
2025-03-09 06:03:19 +0100

    11.04 - Modifying the Recipe Entity Adding Navigation to instructions

--- Wasfat/aspnet-core/src/Wasfat.Domain/Recipes/Recipe.cs
+++ Wasfat/aspnet-core/src/Wasfat.Domain/Recipes/Recipe.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Volo.Abp.Domain.Entities;
+using Wasfat.Instructions;
 
 namespace Wasfat.Recipes
 {
     public class Recipe : Entity<int>
     {
+        public List<Instruction> Instructions { get; set; } = new();
         public string Name { get; set; }
         public string Description { get; set; }
+
     }
 }
