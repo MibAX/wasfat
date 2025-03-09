8e9e02674156201966fec3c62800a3241566ee0d
MibAX <Instructors@mibax.com>
2025-03-09 09:15:50 +0100

    11.15 - Generating the Instructions List Component
        PS> ng generate component instructions/instructions-list

---
+++ Wasfat/admin.angular/src/app/instructions/instructions-list/instructions-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructions-list',
+  templateUrl: './instructions-list.component.html',
+  styleUrls: ['./instructions-list.component.scss']
+})
+export class InstructionsListComponent {
+
+}
--- Wasfat/admin.angular/src/app/instructions/instructions.module.ts
+++ Wasfat/admin.angular/src/app/instructions/instructions.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InstructionsRoutingModule } from './instructions-routing.module';
+import { InstructionsListComponent } from './instructions-list/instructions-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InstructionsListComponent
+  ],
   imports: [
     CommonModule,
     InstructionsRoutingModule
