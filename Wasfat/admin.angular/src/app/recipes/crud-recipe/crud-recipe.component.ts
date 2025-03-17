af709b0647d0d4be634a08710a80654b5a07ef83
Omar Haddad <omar.haddad@mibax.com>
2025-03-17 15:20:03 +0100

    10.20 Refactoring CRUD Recipe for Code Readability

--- Wasfat/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
+++ Wasfat/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
@@ -1,61 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { RecipeAdminService } from '@proxy/recipes';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { RecipeAdminService, RecipeDto } from "@proxy/recipes";
 
 @Component({
-  selector: 'app-crud-recipe',
-  templateUrl: './crud-recipe.component.html',
-  styleUrls: ['./crud-recipe.component.scss']
+  selector: "app-crud-recipe",
+  templateUrl: "./crud-recipe.component.html",
+  styleUrls: ["./crud-recipe.component.scss"],
 })
 export class CrudRecipeComponent implements OnInit {
-  recipeFormGroup: FormGroup;
+  formGroup: FormGroup;
   recipeId: number | null = null;
   isEditMode: boolean = false;
-
 
   constructor(
     private recipeAdminSvc: RecipeAdminService,
     private fb: FormBuilder,
     private router: Router,
-    private activatedRoute: ActivatedRoute) {
-
-  }
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-    console.log('CrudRecipeComponent > ngOnInit')
+    console.log("CrudRecipeComponent > ngOnInit");
     this.buildFrom();
-
-    this.activatedRoute.paramMap.subscribe(params => {
-
-      const idParam = params.get('id');
-
-      if (!idParam) {
-        return;
-      }
-
-      this.recipeId = Number(idParam);
-
-      this.isEditMode = true;
-
-      // load recipe from backend
-      this.recipeAdminSvc.get(this.recipeId).subscribe(response => {
-        // Populate the form with the retrieved recipe data
-        this.recipeFormGroup.patchValue({
-          name: response.name,
-          description: response.description
-        });
-
-      });
-
-
-    })
+    this.patchIfEditMode();
   }
 
   private buildFrom() {
-    this.recipeFormGroup = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      description: ['']
+    this.formGroup = this.fb.group({
+      name: ["", [Validators.required, Validators.minLength(3)]],
+      description: [""],
+    });
+  }
+
+  private patchIfEditMode() {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      const idParam = params.get("id");
+      if (!idParam) return;
+      this.setEditMOde(idParam);
+      this.fetchAndPatch();
     });
   }
 
@@ -63,38 +46,53 @@ export class CrudRecipeComponent implements OnInit {
     this.router.navigate(["/recipes/list"]);
   }
 
-  saveRecipe(): void {
-
-    if (this.recipeFormGroup.invalid) {
-      alert("some Fields are not valid.")
+  save(): void {
+    if (this.formGroup.invalid) {
+      alert("some fields are invalid");
       return;
     }
-
-    if (this.isEditMode && this.recipeId) {
-      // update
-      this.recipeAdminSvc.update(this.recipeId, this.recipeFormGroup.value).subscribe((response) => {
-        console.log('Recipe updated successfully', response);
-        this.router.navigate(["/recipes/list"]);
-      });
+    if (this.isEditMode) {
+      this.update();
     } else {
-      // create
-      this.recipeAdminSvc.create(this.recipeFormGroup.value).subscribe((response) => {
-        console.log('Recipe created successfully', response);
-        this.router.navigate(["/recipes/list"]);
-      });
+      this.create();
     }
-
-
-
   }
 
+  //#region Sub Functions
 
+  private setEditMOde(idParam: string) {
+    this.recipeId = Number(idParam);
+    this.isEditMode = true;
+  }
 
+  private fetchAndPatch() {
+    this.recipeAdminSvc.get(this.recipeId).subscribe((recipe) => {
+      this.patch(recipe);
+    });
+  }
 
+  private patch(response: RecipeDto) {
+    this.formGroup.patchValue({
+      name: response.name,
+      description: response.description,
+    });
+  }
 
+  private update() {
+    this.recipeAdminSvc
+      .update(this.recipeId, this.formGroup.value)
+      .subscribe((response) => {
+        console.log("Recipe updated successfully", response);
+        this.router.navigate(["/recipes/list"]);
+      });
+  }
 
+  private create() {
+    this.recipeAdminSvc.create(this.formGroup.value).subscribe((response) => {
+      console.log("Recipe created successfully", response);
+      this.router.navigate(["/recipes/list"]);
+    });
+  }
 
-
-
-
+  //#endregion
 }
