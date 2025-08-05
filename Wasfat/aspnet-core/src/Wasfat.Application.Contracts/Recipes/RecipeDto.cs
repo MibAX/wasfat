254421c05322c59b5e8cf1f834e269d0b0399015
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-08-05 17:18:28 +0300

    11.12 Modifying RecipeDto to include List<InstructionDto> navigation property

--- Wasfat/aspnet-core/src/Wasfat.Application.Contracts/Recipes/RecipeDto.cs
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/Recipes/RecipeDto.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Volo.Abp.Application.Dtos;
+using Wasfat.Instructions;
 
 namespace Wasfat.Recipes
 {
-
     public class RecipeDto : EntityDto<int>
     {
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public List<InstructionDto> Instructions { get; set; } = new List<InstructionDto>();
     }
 }
