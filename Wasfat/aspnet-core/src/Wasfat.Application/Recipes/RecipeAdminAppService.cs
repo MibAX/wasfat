d3f3c91f3b66be0102ec6566aa7861036bc6cc14
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-08-05 22:08:48 +0300

    11.17 Installing Microsoft.EntityFrameworkCore in the application layer temporarily for educational purposes

--- Wasfat/aspnet-core/src/Wasfat.Application/Recipes/RecipeAdminAppService.cs
+++ Wasfat/aspnet-core/src/Wasfat.Application/Recipes/RecipeAdminAppService.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp;
 using Volo.Abp.Application.Dtos;
