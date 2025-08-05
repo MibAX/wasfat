907853092768d6a9bab3003aef8c126d2f1e1a86
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-08-05 16:54:26 +0300

    11.03 Creating the Instruction entity class

---
+++ Wasfat/aspnet-core/src/Wasfat.Domain/Instructions/Instruction.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace Wasfat.Instructions
+{
+    public class Instruction : Entity<int>
+    {
+        public string Text { get; set; }
+        public int Order { get; set; }
+    }
+}
