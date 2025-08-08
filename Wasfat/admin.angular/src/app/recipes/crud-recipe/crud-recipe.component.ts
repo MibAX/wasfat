9b0ad03aa8292f4e6e7705ffb44249ec9e126843
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-08-08 18:12:11 +0300

    11.29 Simplifying 'id' param retrieval using snapshot over observable

--- Wasfat/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
+++ Wasfat/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
@@ -34,12 +34,10 @@ export class CrudRecipeComponent implements OnInit {
   }
 
   private patchIfEditMode() {
-    this.activatedRoute.paramMap.subscribe(params => {
-      const idParam = params.get('id');
-      if (!idParam) return;
-      this.setEditMode(idParam);
-      this.fetchAndPatch();
-    });
+    const idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!idParam) return;
+    this.setEditMode(idParam);
+    this.fetchAndPatch();
   }
 
   cancel(): void {
