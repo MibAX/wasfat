f65a95fa663c87361fc80469c0eaf0eb4ab82b21
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-08-12 19:12:33 +0300

    11.58 Importing Angular CDK DragDropModule into MaterialModule

--- Wasfat/admin.angular/src/app/shared/material.module.ts
+++ Wasfat/admin.angular/src/app/shared/material.module.ts
@@ -3,19 +3,22 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop'
 
 @NgModule({
   imports: [
     MatFormFieldModule,
     MatButtonModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    DragDropModule
   ],
   exports: [
     MatFormFieldModule,
     MatButtonModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    DragDropModule
   ]
 })
 export class MaterialModule {}
