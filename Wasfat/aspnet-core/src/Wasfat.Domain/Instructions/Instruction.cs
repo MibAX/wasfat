5147af570dfd5af9a91ce1f14037c6a960ccdf03
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-08-16 00:23:26 +0300

    11.04 Establishing One-to-Many Relationship

--- Wasfat/aspnet-core/src/Wasfat.Domain/Instructions/Instruction.cs
+++ Wasfat/aspnet-core/src/Wasfat.Domain/Instructions/Instruction.cs
@@ -1,4 +1,5 @@
 ﻿using Volo.Abp.Domain.Entities;
+using Wasfat.Recipes;
 
 namespace Wasfat.Instructions
 {
@@ -6,5 +7,8 @@ namespace Wasfat.Instructions
     {
         public string Text { get; set; }
         public int Order { get; set; }
+
+        public int RecipeId { get; set; }                   // Foreign key to the Recipe entity
+        public Recipe Recipe { get; set; } = new Recipe();  // Navigation property to the Recipe entity
     }
 }
