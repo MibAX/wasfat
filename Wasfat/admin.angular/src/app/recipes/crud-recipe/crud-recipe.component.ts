8f5d88210e98903e33b6eee6462a56b3906376af
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-08-16 16:49:50 +0300

    11.14 Simplifying Route ID Access Using Snapshot

--- Wasfat/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
+++ Wasfat/admin.angular/src/app/recipes/crud-recipe/crud-recipe.component.ts
@@ -34,14 +34,12 @@ export class CrudRecipeComponent implements OnInit {
   }
 
   private patchIfEditMode() {
-    this.activatedRoute.paramMap.subscribe(params => {
-      const idParam = params.get('id');
-      if (!idParam) return;
-      this.setEditMode(idParam);
-      this.fetchAndPatch();
-    });
-  }
-
+    const idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!idParam) return;
+    this.setEditMode(idParam);
+    this.fetchAndPatch();
+  };
+  
   cancel(): void {
     this.router.navigate(["/recipes/list"]);
   }
