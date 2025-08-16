184f4dc8f747518cca96fdc3abdcea7138d64c94
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-08-17 00:01:02 +0300

    11.30 Adding Drag & Drop for Reordering Instructions

--- Wasfat/admin.angular/src/app/shared/shared.module.ts
+++ Wasfat/admin.angular/src/app/shared/shared.module.ts
@@ -7,9 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-
-
-
+import { DragDropModule } from '@angular/cdk/drag-drop'
 
 @NgModule({
   declarations: [],
@@ -21,7 +19,8 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    DragDropModule
   ],
   exports: [
     CoreModule,
@@ -31,7 +30,8 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    DragDropModule
   ],
   providers: []
 })
