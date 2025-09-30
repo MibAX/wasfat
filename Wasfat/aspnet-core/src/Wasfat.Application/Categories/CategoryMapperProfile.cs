830914181680f4568c60ea96ccadf35496860784
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-09-30 20:01:46 +0300

    12.11 Setting Up the Mapper Profile

---
+++ Wasfat/aspnet-core/src/Wasfat.Application/Categories/CategoryMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Wasfat.Categories
+{
+    public class CategoryMapperProfile : Profile
+    {
+        public CategoryMapperProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
