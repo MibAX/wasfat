1758008c8e8d9c6354987833b92c6daf6383de34
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-02 17:36:06 +0300

    12.26 Initializing Category List Component With Service & OnInit

--- Wasfat/admin.angular/src/app/categories/categories-list/categories-list.component.ts
+++ Wasfat/admin.angular/src/app/categories/categories-list/categories-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryAdminService, CategoryDto } from '@proxy/categories';
 
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.scss']
 })
-export class CategoriesListComponent {
+export class CategoriesListComponent implements OnInit {
+  categories: CategoryDto[] = [];
 
+  constructor(
+    private categoryAdminSvc: CategoryAdminService
+  ) {
+    console.log('CategoriesListComponent > constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('CategoriesListComponent > ngOnInit');
+
+    this.categoryAdminSvc.getAllCategories().subscribe(data => this.categories = data);
+  }
 }
