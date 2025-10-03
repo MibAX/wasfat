1f44adba40e9b94dc5860c2bdc5d9647044f20f1
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-03 19:55:07 +0300

    12.34 Setting Up the CRUD Category Component With Dialog

--- Wasfat/admin.angular/src/app/categories/crud-category/crud-category.component.ts
+++ Wasfat/admin.angular/src/app/categories/crud-category/crud-category.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-crud-category',
   templateUrl: './crud-category.component.html',
   styleUrls: ['./crud-category.component.scss']
 })
-export class CrudCategoryComponent {
-
+export class CrudCategoryComponent implements OnInit {
+  categoryId: number | null = null;
+  
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.categoryId = data.categoryId
+  }
+  
+  ngOnInit(): void {
+    
+  }
 }
