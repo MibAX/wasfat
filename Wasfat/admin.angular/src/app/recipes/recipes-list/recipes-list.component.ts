e7306db7cefc47a8fdcdcf68623fb7d83a986443
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-03 18:39:17 +0300

    12.32 Modifying Recipe List Component to Handle Query Param Filtering

--- Wasfat/admin.angular/src/app/recipes/recipes-list/recipes-list.component.ts
+++ Wasfat/admin.angular/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecipeAdminService, RecipeDto } from '@proxy/recipes';
 
 @Component({
@@ -8,11 +8,11 @@ import { RecipeAdminService, RecipeDto } from '@proxy/recipes';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
-
   recipes: RecipeDto[] = [];
 
   constructor(
     private recipeAdminSvc: RecipeAdminService,
+    private activatedRoute: ActivatedRoute,
     private router: Router) {
     console.log('RecipesListComponent > constructor');
 
@@ -21,9 +21,34 @@ export class RecipesListComponent implements OnInit {
   ngOnInit(): void {
     console.log('RecipesListComponent > ngOnInit');
 
+    this.initialFetch();
+  }
+
+  private initialFetch(): void {
+    const categoryId = Number(this.activatedRoute.snapshot.queryParamMap.get('categoryId'));
+    if(categoryId) {
+      this.getFilteredRecipes(categoryId);
+    }
+    else {
+      this.getAllRecipes();
+    }
+  }
+
+  private getAllRecipes(): void {
     this.recipeAdminSvc.getAllRecipes().subscribe(data => this.recipes = data);
+  }
 
+  private getFilteredRecipes(categoryId: number): void {
+    this.recipeAdminSvc.getFiltered(categoryId).subscribe(data => this.recipes = data)
+  }
 
+  resetFilters(): void {
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: {}, // empty object
+    });
+
+    this.getAllRecipes();
   }
 
   newRecipe(): void {
@@ -33,5 +58,4 @@ export class RecipesListComponent implements OnInit {
   editRecipe(id: number): void {
     this.router.navigate(["/recipes/edit", id]);
   }
-
 }
