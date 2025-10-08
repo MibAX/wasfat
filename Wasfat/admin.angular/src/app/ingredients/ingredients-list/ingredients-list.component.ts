753aefda8444d0c4b2df5f8e8f0638124cdfaaea
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-08 18:22:36 +0300

    13.31 Integrating `Mat Dialog` in Ingredients List Component

--- Wasfat/admin.angular/src/app/ingredients/ingredients-list/ingredients-list.component.ts
+++ Wasfat/admin.angular/src/app/ingredients/ingredients-list/ingredients-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { IngredientAdminService, IngredientDto } from '@proxy/ingredients';
+import { CrudIngredientComponent } from '../crud-ingredient/crud-ingredient.component';
 
 @Component({
   selector: 'app-ingredients-list',
@@ -10,14 +12,29 @@ export class IngredientsListComponent implements OnInit {
   ingredients: IngredientDto[] = [];
 
   constructor(
-    private ingredientAdminSvc: IngredientAdminService
+    private ingredientAdminSvc: IngredientAdminService,
+    private dialog: MatDialog
   ) {
-    console.log('CategoriesListComponent > constructor');
+    console.log('IngredientsListComponent > constructor');
   }
 
   ngOnInit(): void {
-    console.log('CategoriesListComponent > ngOnInit');
+    console.log('IngredientsListComponent > ngOnInit');
 
+    this.getAllCategories();
+  }
+
+  private getAllCategories(): void {
     this.ingredientAdminSvc.getAllIngredients().subscribe(data => this.ingredients = data);
+  }
+
+  openDialog(ingredientId?: number): void {
+    const dialogRef = this.dialog.open(CrudIngredientComponent, {
+      width: '20rem',
+      disableClose: true,
+      data: { ingredientId: ingredientId }
+    })
+
+    dialogRef.afterClosed().subscribe(() => this.getAllCategories())
   }
 }
