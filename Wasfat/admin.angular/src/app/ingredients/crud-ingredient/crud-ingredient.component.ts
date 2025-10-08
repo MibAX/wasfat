d7ea91bb75bef5fa3ca26d5a01b913314baeb692
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-08 18:26:24 +0300

    13.32 Setting Up the CRUD Ingredient Component With Dialog

--- Wasfat/admin.angular/src/app/ingredients/crud-ingredient/crud-ingredient.component.ts
+++ Wasfat/admin.angular/src/app/ingredients/crud-ingredient/crud-ingredient.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-crud-ingredient',
   templateUrl: './crud-ingredient.component.html',
   styleUrls: ['./crud-ingredient.component.scss']
 })
-export class CrudIngredientComponent {
+export class CrudIngredientComponent implements OnInit {
+  ingredientId: number | null = null;
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.ingredientId = data.ingredientId
+  }
+
+  ngOnInit(): void {
+
+  }
 }
