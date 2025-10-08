c139f801f4837d81361a57f3960eb46b0298f4e6
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-08 13:36:39 +0300

    13.11 Creating the DTOs

---
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/Ingredients/IngredientDto.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace Wasfat.Ingredients
+{
+    public class IngredientDto : EntityDto<int>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/RecipeIngredients/CrudRecipeIngredientDto.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace Wasfat.RecipeIngredients
+{
+    public class CrudRecipeIngredientDto : EntityDto
+    {
+        public int RecipeId { get; set; }
+        public int IngredientId { get; set; }
+
+        public decimal Quantity { get; set; }
+        public MeasurementUnit Unit { get; set; }
+    }
+}
