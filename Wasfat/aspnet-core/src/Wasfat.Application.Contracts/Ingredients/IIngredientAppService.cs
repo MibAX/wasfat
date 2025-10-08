63e33ff90f4cf0b275859196c66072404e0291c9
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-08 13:57:50 +0300

    13.14 Defining the Ingredient Service Interface

---
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/Ingredients/IIngredientAppService.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Wasfat.Ingredients
+{
+    public interface IIngredientAppService : ICrudAppService<
+        IngredientDto,
+        int,
+        PagedAndSortedResultRequestDto>
+    {
+    }
+}
