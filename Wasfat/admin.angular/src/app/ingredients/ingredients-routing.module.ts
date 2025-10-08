963c9d8238fa6b8a91cc884970fcfd5c4f10b3c4
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-08 17:40:30 +0300

    13.23 Routing Inside the Ingredients Module

--- Wasfat/admin.angular/src/app/ingredients/ingredients-routing.module.ts
+++ Wasfat/admin.angular/src/app/ingredients/ingredients-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngredientsListComponent } from './ingredients-list/ingredients-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',
+    component: IngredientsListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
