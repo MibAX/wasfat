2c38f94b9b814e461b5586eedffa838ad5e5ad02
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-08 13:56:37 +0300

    13.13 Setting Up the Mapper Profiles

---
+++ Wasfat/aspnet-core/src/Wasfat.Application/Ingredients/IngredientMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Wasfat.Ingredients
+{
+    public class IngredientMapperProfile : Profile
+    {
+        public IngredientMapperProfile()
+        {
+            CreateMap<Ingredient, IngredientDto>().ReverseMap();
+        }
+    }
+}
---
+++ Wasfat/aspnet-core/src/Wasfat.Application/RecipeIngredients/RecipeIngredientMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace Wasfat.RecipeIngredients
+{
+    public class RecipeIngredientMapperProfile : Profile
+    {
+        public RecipeIngredientMapperProfile()
+        {
+            CreateMap<RecipeIngredient, RecipeIngredientDto>();
+            CreateMap<RecipeIngredient, CrudRecipeIngredientDto>().ReverseMap();
+        }
+    }
+}
