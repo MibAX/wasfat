5c98228bcbe8b11bbaab09d9beec94bdbbae789e
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-08 13:32:04 +0300

    13.09 Adding Migration

---
+++ Wasfat/aspnet-core/src/Wasfat.EntityFrameworkCore/Migrations/20251008103113_AddIngredientAndRecipeIngredientTablesWithRelation.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wasfat.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIngredientAndRecipeIngredientTablesWithRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppIngredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppIngredients", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AppRecipeIngredients",
+                columns: table => new
+                {
+                    RecipeId = table.Column<int>(type: "int", nullable: false),
+                    IngredientId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Unit = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppRecipeIngredients", x => new { x.RecipeId, x.IngredientId });
+                    table.ForeignKey(
+                        name: "FK_AppRecipeIngredients_AppIngredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "AppIngredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppRecipeIngredients_AppRecipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "AppRecipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppRecipeIngredients_IngredientId",
+                table: "AppRecipeIngredients",
+                column: "IngredientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppRecipeIngredients");
+
+            migrationBuilder.DropTable(
+                name: "AppIngredients");
+        }
+    }
+}
