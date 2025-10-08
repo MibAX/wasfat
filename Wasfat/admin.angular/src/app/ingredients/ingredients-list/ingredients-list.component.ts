29fcf803bf9483c348d0882957f6f6dd5d6948cc
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-10-08 17:54:59 +0300

    13.29 Initializing Ingredient List Component With Service & OnInit

--- Wasfat/admin.angular/src/app/ingredients/ingredients-list/ingredients-list.component.ts
+++ Wasfat/admin.angular/src/app/ingredients/ingredients-list/ingredients-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IngredientAdminService, IngredientDto } from '@proxy/ingredients';
 
 @Component({
   selector: 'app-ingredients-list',
   templateUrl: './ingredients-list.component.html',
   styleUrls: ['./ingredients-list.component.scss']
 })
-export class IngredientsListComponent {
+export class IngredientsListComponent implements OnInit {
+  ingredients: IngredientDto[] = [];
 
+  constructor(
+    private ingredientAdminSvc: IngredientAdminService
+  ) {
+    console.log('CategoriesListComponent > constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('CategoriesListComponent > ngOnInit');
+
+    this.ingredientAdminSvc.getAllIngredients().subscribe(data => this.ingredients = data);
+  }
 }
