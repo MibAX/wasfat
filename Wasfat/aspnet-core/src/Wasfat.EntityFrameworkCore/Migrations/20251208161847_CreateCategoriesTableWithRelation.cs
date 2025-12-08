9cf62def7e3441a3080af404dda8ee167eb68600
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-12-08 19:19:59 +0300

    12.07 Adding a Migration

---
+++ Wasfat/aspnet-core/src/Wasfat.EntityFrameworkCore/Migrations/20251208161847_CreateCategoriesTableWithRelation.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wasfat.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCategoriesTableWithRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppCategories", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AppRecipeCategories",
+                columns: table => new
+                {
+                    CategoriesId = table.Column<int>(type: "int", nullable: false),
+                    RecipesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppRecipeCategories", x => new { x.CategoriesId, x.RecipesId });
+                    table.ForeignKey(
+                        name: "FK_AppRecipeCategories_AppCategories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "AppCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppRecipeCategories_AppRecipes_RecipesId",
+                        column: x => x.RecipesId,
+                        principalTable: "AppRecipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppRecipeCategories_RecipesId",
+                table: "AppRecipeCategories",
+                column: "RecipesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppRecipeCategories");
+
+            migrationBuilder.DropTable(
+                name: "AppCategories");
+        }
+    }
+}
