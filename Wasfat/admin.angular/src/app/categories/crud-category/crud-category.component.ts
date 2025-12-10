116c7b8645c4a7a8ab361a2b43a4ce46157e27ed
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2025-12-10 15:50:37 +0300

    12.39 Declaring the `CategoryFormGroup`

--- Wasfat/admin.angular/src/app/categories/crud-category/crud-category.component.ts
+++ Wasfat/admin.angular/src/app/categories/crud-category/crud-category.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-crud-category',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./crud-category.component.scss']
 })
 export class CrudCategoryComponent {
-
+  categoryFormGroup: FormGroup;
 }
