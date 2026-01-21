3536a282dfbaa6625504b6ff862b80f1d7246eb7
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-01-21 15:18:40 +0300

    13.12 Creating the RecipeIngreidnet (DTO)

---
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/RecipeIngredients/RecipeIngredientDto.cs
@@ -0,0 +1,18 @@
+﻿using Volo.Abp.Application.Dtos;
+using Wasfat.Ingredients;
+using Wasfat.Recipes;
+
+namespace Wasfat.RecipeIngredients
+{
+    public class RecipeIngredientDto : EntityDto
+    {
+        public int RecipeId { get; set; }
+        public int IngredientId { get; set; }
+
+        public decimal Quantity { get; set; }
+        public MeasurementUnit Unit { get; set; }
+
+        public RecipeDto Recipe { get; set; }
+        public IngredientDto Ingredient { get; set; }
+    }
+}
