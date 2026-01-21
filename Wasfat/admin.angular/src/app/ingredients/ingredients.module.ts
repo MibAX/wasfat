e3d8b7e23da624673386df533077ce6f1b574fc2
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-01-21 16:20:11 +0300

    13.18 Generating the Ingredients Module

---
+++ Wasfat/admin.angular/src/app/ingredients/ingredients.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IngredientsRoutingModule } from './ingredients-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IngredientsRoutingModule,
+    SharedModule
+  ]
+})
+export class IngredientsModule { }
