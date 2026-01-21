d6507adb72e5d4a7abebbc3e69ca25ab27cec89a
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-01-21 16:01:36 +0300

    13.16 Implementing the Ingredient Service

---
+++ Wasfat/aspnet-core/src/Wasfat.Application/Ingredients/IngredientAdminAppService.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Wasfat.Ingredients
+{
+    public class IngredientAdminAppService : CrudAppService<Ingredient, IngredientDto, int, PagedAndSortedResultRequestDto>, IIngredientAppService
+    {
+        public IngredientAdminAppService(
+            IRepository<Ingredient, int> ingredientsRepository
+            )
+        : base(ingredientsRepository)
+        {
+
+        }
+    }
+}
