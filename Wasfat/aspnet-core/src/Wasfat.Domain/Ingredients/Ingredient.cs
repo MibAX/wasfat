680a319d975c178257b7a8c13bd767791e9bbbb5
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-01-21 14:46:18 +0300

    13.06 Establishing Many-to-Many Relationship With Junction-Level Props

--- Wasfat/aspnet-core/src/Wasfat.Domain/Ingredients/Ingredient.cs
+++ Wasfat/aspnet-core/src/Wasfat.Domain/Ingredients/Ingredient.cs
@@ -1,9 +1,13 @@
-﻿using Volo.Abp.Domain.Entities;
+﻿using System.Collections.Generic;
+using Volo.Abp.Domain.Entities;
+using Wasfat.RecipeIngredients;
 
 namespace Wasfat.Ingredients
 {
     public class Ingredient : Entity<int>
     {
         public string Name { get; set; }
+
+        public List<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>(); // Navigation property to the RecipeIngredient entity
     }
 }
--- Wasfat/aspnet-core/src/Wasfat.Domain/RecipeIngredients/RecipeIngredient.cs
+++ Wasfat/aspnet-core/src/Wasfat.Domain/RecipeIngredients/RecipeIngredient.cs
@@ -1,8 +1,23 @@
-﻿namespace Wasfat.RecipeIngredients
+﻿using Volo.Abp.Domain.Entities;
+using Wasfat.Ingredients;
+using Wasfat.Recipes;
+
+namespace Wasfat.RecipeIngredients
 {
-    public class RecipeIngredient
+    public class RecipeIngredient : Entity
     {
+        public int RecipeId { get; set; }
+        public int IngredientId { get; set; }
+
         public decimal Quantity { get; set; }
         public MeasurementUnit Unit { get; set; }
+
+        public Recipe Recipe { get; set; } // Navigation property to the Recipe entity
+        public Ingredient Ingredient { get; set; } // Navigation property to the Ingredient entity
+
+        public override object?[] GetKeys()
+        {
+            return new object?[] { RecipeId, IngredientId };
+        }
     }
 }
