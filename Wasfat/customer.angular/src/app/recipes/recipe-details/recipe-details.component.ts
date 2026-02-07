63bae5aa712a09cb2053b353a23417e5230ef215
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-02-07 21:46:32 +0300

    feat(ui): Working solution!

--- Wasfat/customer.angular/src/app/recipes/recipe-details/recipe-details.component.ts
+++ Wasfat/customer.angular/src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { EnvironmentService } from '@abp/ng.core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LookupDto } from '@proxy/common';
+import { IngredientAdminService } from '@proxy/ingredients';
+import { measurementUnitOptions } from '@proxy/recipe-ingredients';
+import { RecipeDto, RecipeAdminService } from '@proxy/recipes';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
   styleUrls: ['./recipe-details.component.scss']
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
+  apiUrl: string;
+  recipe: RecipeDto;
+  ingredientLookups: LookupDto[];
+  measurementUnitOptions = measurementUnitOptions;
+  
+  constructor(
+    private recipeSvc: RecipeAdminService, 
+    private ingredientSvc: IngredientAdminService,
+    private activatedRoute: ActivatedRoute,
+    private environmentSvc: EnvironmentService
+  ) {
+    console.log('RecipeDetailsComponent > constructor');
+  }
 
+  ngOnInit(): void {
+    this.getApiUrl();
+    this.getRecipe();
+    this.getIngredientLookups();
+  }
+
+  private getApiUrl(): void {
+    this.apiUrl = this.environmentSvc.getApiUrl('default');
+  }
+
+  private getRecipe(): void {
+    const idParam = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.recipeSvc.get(idParam).subscribe(response => this.recipe = response);
+  }
+
+  private getIngredientLookups(): void {
+    this.ingredientSvc.getLookups().subscribe(response => this.ingredientLookups = response);
+  }
+
+  getIngredientLabel(ingredientId: number): string {
+    return this.ingredientLookups?.find(c => c.id === ingredientId).displayName;
+  }
+
+  getMeasurementUnitName(value: number): string {
+  return this.measurementUnitOptions.find(o => o.value === value)?.key ?? '';
+}
 }
--- Wasfat/customer.angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Wasfat/customer.angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { EnvironmentService } from '@abp/ng.core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { RecipeDto, RecipeAdminService } from '@proxy/recipes';
+import { startWith, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  apiUrl: string;
+  searchForm: FormGroup;
+  recipes: RecipeDto[];
+  filteredRecipes: RecipeDto[];
 
+  get recipeNameControl(): FormControl {
+    return this.searchForm.get('recipeName') as FormControl;
+  }
+
+  constructor(
+    private recipeSvc: RecipeAdminService,
+    private fb: FormBuilder,
+    private environmentSvc: EnvironmentService
+  ) {}
+
+  ngOnInit(): void {
+    this.getApiUrl();
+    this.buildForm();
+    this.setupFiltering();
+    this.getRecipes();
+  }
+
+  private getApiUrl(): void {
+    this.apiUrl = this.environmentSvc.getApiUrl('default');
+  }
+
+  private buildForm(): void {
+    this.searchForm = this.fb.group({
+      recipeName: ['']
+    });
+  }
+
+  private getRecipes(): void {
+    this.recipeSvc.getAllRecipes().subscribe(recipes => {
+      this.recipes = recipes;
+    });
+  }
+
+  private setupFiltering(): void {
+    this.recipeNameControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300)
+    ).subscribe(searchKey => {
+
+      if (!searchKey || !searchKey.trim()) {
+        this.filteredRecipes = this.recipes;
+        return;
+      }
+
+      const key = searchKey.toLowerCase();
+
+      this.filteredRecipes = this.recipes.filter(recipe =>
+        recipe.name?.toLowerCase().includes(key)
+      );
+    });
+  }
 }
