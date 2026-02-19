04ea8ac204e660a847d6fe38d41cafe1ad4191bd
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-02-19 14:36:21 +0300

    14.09 Updating the Customer Angular Environment Settings

--- Wasfat/customer.angular/src/environments/environment.ts
+++ Wasfat/customer.angular/src/environments/environment.ts
@@ -1,18 +1,18 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:4200';
+const baseUrl = 'http://localhost:4300';
 
 export const environment = {
   production: false,
   application: {
     baseUrl,
-    name: 'Wasfat',
+    name: 'Wasfat-Customer',
     logoUrl: '',
   },
   oAuthConfig: {
     issuer: 'https://localhost:44346/',
     redirectUri: baseUrl,
-    clientId: 'Wasfat_App',
+    clientId: 'Wasfat_Customer',
     responseType: 'code',
     scope: 'offline_access Wasfat',
     requireHttps: true,
