74d605692100688e7eb6a577b7f04f68600ea713
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-03-01 16:46:17 +0300

    feat(recipe-list): listed all recipes in simple cards

--- Wasfat/customer.angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Wasfat/customer.angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeAdminService, RecipeDto } from '@proxy/recipes';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: RecipeDto[];
 
+  constructor(private recipeSvc: RecipeAdminService) {}
+
+  ngOnInit(): void {
+    this.getRecipes();
+  }
+  
+  private getRecipes(): void {
+    this.recipeSvc.getAllRecipes().subscribe(result => this.recipes = result);
+  }
 }
