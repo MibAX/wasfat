48fe8b238f8b0507d5d2793b120d597319bec885
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-03-02 21:40:33 +0300

    feat(recipe): generated the recipe-details component

--- Wasfat/customer.angular/src/app/recipes/recipes-routing.module.ts
+++ Wasfat/customer.angular/src/app/recipes/recipes-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 const routes: Routes = [
   {
     path: 'list', component: RecipeListComponent
+  },
+  {
+    path: 'details/:id', component: RecipeDetailsComponent
   }
 ];
 
--- Wasfat/customer.angular/src/app/recipes/recipes.module.ts
+++ Wasfat/customer.angular/src/app/recipes/recipes.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 
 @NgModule({
   declarations: [
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     CommonModule,
