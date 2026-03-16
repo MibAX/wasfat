ec669c86750c55860d1ee00bcafd845bb8bdb00e
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-03-16 16:04:18 +0300

    Video_3-4

---
+++ Wasfat/aspnet-core/src/Wasfat.Domain/Instructions/Instruction.cs
@@ -0,0 +1,14 @@
+﻿using Volo.Abp.Domain.Entities;
+using Wasfat.Recipes;
+
+namespace Wasfat.Instructions
+{
+    public class Instruction : Entity<int>
+    {
+        public string Text { get; set; } 
+        public int Order { get; set; }
+
+        public Recipe Recipe { get; set; }
+        public int RecipeId { get; set; }   
+    }
+}
