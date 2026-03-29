2d84c3b3fe9cc18f814618d24d067ffad62563e3
Abdullah Al-Shishani <abdullah.alshishani@abatchi.com>
2026-03-29 16:31:09 +0300

    Video-9

---
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/Instructions/InstructionDto.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace Wasfat.Instructions
+{
+    public class InstructionDto : EntityDto<int>
+    {
+        public string Text { get; set; }
+        public int Order { get; set; }
+
+        public int RecipeId { get; set; }
+    }
+}
